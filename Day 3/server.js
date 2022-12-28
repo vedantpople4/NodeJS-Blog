63454f2c5506fd5c0eacaf1919608d9fa48b9e95
Vedant pople <vedantpople4@gmail.com>
2022-12-28 21:37:15 +0530

    Client and Servers

---
+++ Day 3/server.js
@@ -0,0 +1,45 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    //set header constant type
+    res.setHeader('Content-Type','text/html');
+
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', 'about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+    }
+
+    fs.readFile(path, (err,data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        } else {
+            //res.write(data);
+            res.end(data);
+        } 
+    })
+});
+
+server.listen(3000, 'localhost', () =>{
+    console.log('listening request on port 3000');
+});
+
