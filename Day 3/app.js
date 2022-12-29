d55262efe0ef795eca7bdfd6f5ebe4ff60898084
Vedant pople <vedantpople4@gmail.com>
2022-12-29 22:21:20 +0530

    EJS done

--- Day 3/app.js
+++ Day 3/app.js
@@ -1,25 +1,33 @@
 const express = require('express');
+const { create } = require('lodash');
 
 const app = express();
 
+// view engines
+app.set('view engine', 'ejs');
 //listening for requests
 app.listen(3000);
 
 app.get('/', (req,res)=>{
+    const blogs = [
+        {title : 'RedBull', snippet: 'Redbull is F1 team'},
+        {title : 'Mercedes', snippet: 'Mercedes is F1 team'},
+        {title : 'Ferrari', snippet: 'Ferrari is F1 team'},
+    ];
    // res.send('<p>This is it</p>')
-   res.sendFile('./views/index.html', {root: __dirname});
+   res.render('index', { title : 'Home', blogs});
 });
 
 app.get('/about', (req,res)=>{
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', { title : 'About'});
 });
 
 //redirects
-app.get('/about-us',(req,res) => {
-    res.redirect('/about');
-});
+app.get('/blogs/create', (req,res)=>{
+    res.render('create', { title : 'Create'});
+})
 
 //404 page
 app.use((req, res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404', { title : 'Error'});
 });  
